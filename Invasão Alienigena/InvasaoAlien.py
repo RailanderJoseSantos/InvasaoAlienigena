ec70168754676dd1851e5990ea9efa42e50a67da
RailanderJoseSantos <36487179+railanderjosesantos@users.noreply.github.com>
2022-09-30 21:46:43 -0300

    funcao principal

---
+++ Invasão Alienigena/InvasaoAlien.py
@@ -0,0 +1,18 @@
+import sys
+import pygame as pg
+def runGame():
+    """Inicializa o jogo e cria um objeto para tela"""
+    pg.init()
+    #setando display para tela
+    sreen =  pg.display.set_mode((1200,800))
+    pg.display.set_caption("Invasão Alien")
+
+    #laço principal do jogo
+    while True:
+        #escuta evento de teclado e mouse
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+        # deixa tela mais recente visivel
+        pg.display.flip()
+    runGame()
