387a608a372667e3714c4b11b2e93e5279bb4899
RailanderJoseSantos <36487179+RailanderJoseSantos@users.noreply.github.com>
2022-10-02 13:23:32 -0300

    incluido a imagem da nave (estatica)

--- Invasão Alienigena/InvasaoAlien.py
+++ Invasão Alienigena/InvasaoAlien.py
@@ -1,13 +1,15 @@
 import sys
 import pygame as pg
 from settings import Settings
+from ship import Ship
 def runGame():
-    aiSettings = Settings()
     "Inicializa o jogo e cria um objeto para tela"
     pg.init()
-    #setando display para tela  
-    screen =  pg.display.set_mode((aiSettings.screenWidth,aiSettings.escreenHeidth))
+    aiSettings = Settings()
+    #setando display para tela
+    screen = pg.display.set_mode((aiSettings.screenWidth,aiSettings.escreenHeidth))
     pg.display.set_caption("Invasão Alien")
+    nave = Ship(screen)
 
     #laço principal do jogo
     while True:
@@ -17,7 +19,7 @@ def runGame():
                 sys.exit()
         #redesenha a tela a cada passada pelo laço
         screen.fill(aiSettings.bgColor)
-
+        nave.blitme()
         # deixa tela mais recente visivel
         pg.display.flip()
 runGame()
--- Invasão Alienigena/ship.py
+++ Invasão Alienigena/ship.py
@@ -7,3 +7,14 @@ class Ship():
 
         #carrega imagem da nave e obtem seu rect
         self.image = pygame.image.load('image/ship.bmp')
+        # a imagem é inserida num retangulo, assim o pygame sabera omde
+        # inicia a imagem da nave
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #inicia cada nova espaconave em sua posicao atual
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+    def blitme(self):
+        """Desenha a espaconave em sua posicao atual"""
+        self.screen.blit(self.image, self.rect)
